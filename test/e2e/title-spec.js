92968ab6bf6066df65cff7766e94ec7ea4572608
twflu <yzzhou@thoughtworks.com>
2014-05-03 22:17:20 +0800

    [#N/A][yzzhou] - model, binding repeat

--- test/e2e/title-spec.js
+++ test/e2e/title-spec.js
@@ -1,17 +1,19 @@
-describe('title', function(){
+describe('protractor test', function(){
     beforeEach(function () {
         browser.get('http://127.0.0.1:9000/#/');
     });
 
-    xit('should return the title', function(){
+    it('should return the title', function(){
         var nameField = element(by.model('name'));
         var nameLabel = element(by.binding('name'));
         nameField.sendKeys('yzzhou');
-
         expect(nameLabel.getText()).toBe('yzzhou');
+
+        var h1 = element(by.css('h1'));
+        expect(h1.getText()).toBe('yzzhou1');
     });
 
-    it('should', function(){
+    it('repeater', function(){
         var firstLine = element(by.repeater('thing in awesomeThings').row(0));
         var secondLine = element(by.repeater('thing in awesomeThings').row(1));
         expect(firstLine.getText()).toBe('HTML5 Boilerplate');
