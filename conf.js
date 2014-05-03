ce8cb3da1aaa64c60aa037f60f1382605f2c6d2d
twflu <yzzhou@thoughtworks.com>
2014-05-03 22:19:50 +0800

    [#N/A][yzzhou] - rename file name.

--- conf.js
+++ conf.js
@@ -10,7 +10,7 @@ exports.config = {
 
   // Spec patterns are relative to the current working directly when
   // protractor is called.
-  specs: ['test/e2e/title-spec.js'],
+  specs: ['test/e2e/showcase-spec.js'],
 
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
